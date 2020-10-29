1b0dff75dbf1ca5cc524991b78443d290344f61d
paulobenites <anazco.paulo@gmail.com>
2020-10-29 16:10:04 -0300

    Client Model Class

---
+++ src/Model/cliente.java
@@ -0,0 +1,64 @@
+
+package Model;
+
+/**
+ *
+ * @author paulo
+ */
+public class cliente {
+    
+    private int id;
+    private String name;
+    private String rg;
+    private String email;
+    private String telefone;
+    private String celular;
+    private String cep;
+    private String endereco;
+    private int numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+    
+}
