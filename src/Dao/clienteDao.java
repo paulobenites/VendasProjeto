177355c6f4ca4d5c2903b72faa2f6cde430947fc
paulobenites <anazco.paulo@gmail.com>
2020-11-03 11:14:29 -0300

    Implement Method client list

--- src/Dao/clienteDao.java
+++ src/Dao/clienteDao.java
@@ -5,46 +5,84 @@ import Model.cliente;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class clienteDao {
-    
+
     private Connection con;
-    
-    public clienteDao(){
-    this.con = new conectionFactory().getConnetion();
+
+    public clienteDao() {
+        this.con = new conectionFactory().getConnetion();
     }
-    
-    public void cadastrarCliente(cliente obj){
+
+    public void cadastrarCliente(cliente obj) {
         try {
             String sql = "insert into tb_clientes(nome,rg,cpf,email,telefone,celular,cep,endereco,numero,complemento,bairro,cidade,estado)"
-                                                 +"values(?,?,?,?,?,?,?,?,?,?,?,?,?) ";
-            
+                    + "values(?,?,?,?,?,?,?,?,?,?,?,?,?) ";
+
             PreparedStatement stmt = con.prepareStatement(sql);
-            stmt.setString(1,obj.getName());
-            stmt.setString(2,obj.getRg());
-            stmt.setString(3,obj.getCpf());
-            stmt.setString(4,obj.getEmail());
-            stmt.setString(5,obj.getTelefone());
-            stmt.setString(6,obj.getCelular());
-            stmt.setString(7,obj.getCep());
-            stmt.setString(8,obj.getEndereco());
-            stmt.setInt(9,obj.getNumero());
-            stmt.setString(10,obj.getComplemento());
-            stmt.setString(11,obj.getBairro());
-            stmt.setString(12,obj.getCidade());
-            stmt.setString(13,obj.getUf());
-            
+            stmt.setString(1, obj.getName());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getTelefone());
+            stmt.setString(6, obj.getCelular());
+            stmt.setString(7, obj.getCep());
+            stmt.setString(8, obj.getEndereco());
+            stmt.setInt(9, obj.getNumero());
+            stmt.setString(10, obj.getComplemento());
+            stmt.setString(11, obj.getBairro());
+            stmt.setString(12, obj.getCidade());
+            stmt.setString(13, obj.getUf());
+
             stmt.execute();
             stmt.close();
-            
-            JOptionPane.showInputDialog(null,"Cadastro com Sucesso!");
+
+            JOptionPane.showInputDialog(null, "Cadastro com Sucesso!");
         } catch (SQLException erro) {
-            JOptionPane.showInputDialog(null,"Erro!"+erro);
+            JOptionPane.showInputDialog(null, "Erro!" + erro);
         }
     }
-    public void alterarCliente(){
+
+    public void alterarCliente() {
     }
-    public void excluirCliente(){
+
+    public void excluirCliente() {
+    }
+
+    public List<cliente> listaClientes() {
+        try {
+            List<cliente> lista = new ArrayList<>();
+            String sql = "Select * from tb_clientes";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                cliente obj = new cliente();
+                obj.setId(rs.getInt("id"));
+                obj.setName(rs.getString("nome"));
+                obj.setRg(rs.getString("rg"));
+                obj.setCpf(rs.getString("cpf"));
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("uf"));
+
+                lista.add(obj);
+            }
+            return lista;
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro" + erro);
+            return null;
+        }
+
     }
 }
