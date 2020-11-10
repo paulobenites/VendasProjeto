ac634c4e8b1db531cf17ae4e89ec89b3d441baf3
paulobenites <anazco.paulo@gmail.com>
2020-11-10 11:19:34 -0300

    Employees Model Class

---
+++ src/Model/funcionarios.java
@@ -0,0 +1,40 @@
+
+package Model;
+
+/**
+ *
+ * @author paulo
+ */
+public class funcionarios extends cliente{
+    
+    private String senha;
+    private String cargo;
+    private String nivelAcesso;
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getNivelAcesso() {
+        return nivelAcesso;
+    }
+
+    public void setNivelAcesso(String nivelAcesso) {
+        this.nivelAcesso = nivelAcesso;
+    }
+    
+    
+    
+}
