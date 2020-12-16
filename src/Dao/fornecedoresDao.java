8b51719b24887b9afd06f1120e0c56f77130beea
paulobenites <anazco.paulo@gmail.com>
2020-12-16 16:14:00 -0300

    Creat Methods Consult/Search by Name and Providers List

--- src/Dao/fornecedoresDao.java
+++ src/Dao/fornecedoresDao.java
@@ -11,7 +11,10 @@ import Model.fornecedores;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
+import java.sql.ResultSet;
 
 /**
  *
@@ -93,6 +96,102 @@ public class fornecedoresDao {
             JOptionPane.showInputDialog(null, "Alterado com Sucesso!");
         } catch (SQLException erro) {
             JOptionPane.showInputDialog(null, "Erro!" + erro);
+        }
+    }
+    
+    public List<fornecedores> listarFornrcedores() {
+        try {
+            List<fornecedores> lista = new ArrayList<>();
+            String sql = "Select * from tb_clientes";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                fornecedores obj = new fornecedores();
+                obj.setId(rs.getInt("id"));
+                obj.setName(rs.getString("nome"));
+                obj.setCnpj(rs.getString("cnpj"));
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("estado"));
+
+                lista.add(obj);
+            }
+            return lista;
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro" + erro);
+            return null;
+        }
+
+    }
+    
+    public List<fornecedores> buscarFornecedorsPorNome(String nome) {
+        try {
+            List<fornecedores> lista = new ArrayList<>();
+            String sql = "Select * from tb_fornecedores where nome like ?";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, nome);
+            ResultSet rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                fornecedores obj = new fornecedores();
+                obj.setId(rs.getInt("id"));
+                obj.setName(rs.getString("nome"));
+                obj.setCnpj(rs.getString("cnpj"));
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("estado"));
+
+                lista.add(obj);
+            }
+            return lista;
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro" + erro);
+            return null;
+        }
+    }
+    
+     public fornecedores consultaFornecedoresPorNome(String nome){
+        try {
+            String sql = "Select * from tb_clientes where nome = ?";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, nome);
+            ResultSet rs = stmt.executeQuery();
+            fornecedores obj = new fornecedores();
+            
+            if(rs.next()) {
+                obj.setId(rs.getInt("id"));
+                obj.setName(rs.getString("nome"));
+                obj.setCnpj(rs.getString("cnpj"));
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("estado"));
+            }
+            return obj;
+            
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Cliente não Encontrado" + erro );
+            return null;
         }
     }
 }
