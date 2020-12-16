54166ea8d10b1e64d98f445b6038f22ab81fad62
paulobenites <anazco.paulo@gmail.com>
2020-12-16 15:20:58 -0300

    Creat Methods Change and Delete in ProvidersDao

--- src/Dao/fornecedoresDao.java
+++ src/Dao/fornecedoresDao.java
@@ -52,4 +52,47 @@ public class fornecedoresDao {
             JOptionPane.showInputDialog(null, "Erro!" + erro);
         }
     }
+    
+    public void excluirFornecedores(fornecedores obj){
+         try {
+            String sql = "delete from tb_fornecedores where id = ?";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, obj.getId());
+            
+            stmt.execute();
+            stmt.close();
+            
+            JOptionPane.showMessageDialog(null, "Excluido com sucesso!");
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro" + erro);
+        }
+    }
+     public void alterarFornecedores(fornecedores obj) {
+        try {
+            String sql = "update tb_fornecedores set nome =?,cnpj =?,email =?,telefone =?,celular =?,cep =?,endereco =?,numero =?,complemento =?,bairro =?,cidade =?,estado =? where id =?";
+                   
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, obj.getName());
+            stmt.setString(2, obj.getCnpj());
+            stmt.setString(3, obj.getEmail());
+            stmt.setString(4, obj.getTelefone());
+            stmt.setString(5, obj.getCelular());
+            stmt.setString(6, obj.getCep());
+            stmt.setString(7, obj.getEndereco());
+            stmt.setInt(8, obj.getNumero());
+            stmt.setString(9, obj.getComplemento());
+            stmt.setString(10, obj.getBairro());
+            stmt.setString(11, obj.getCidade());
+            stmt.setString(12, obj.getUf());
+            stmt.setInt(13, obj.getId());
+
+            stmt.execute();
+            stmt.close();
+
+            JOptionPane.showInputDialog(null, "Alterado com Sucesso!");
+        } catch (SQLException erro) {
+            JOptionPane.showInputDialog(null, "Erro!" + erro);
+        }
+    }
 }
